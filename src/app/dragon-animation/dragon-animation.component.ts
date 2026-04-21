d6175223bc20ec0871e078fc04df200a2ac5bf33
Carlos <kaduce2009@hotmail.com>
2026-04-21 05:57:44 -0300

    [FIX] - 22 Dragão na tela de carregamento

--- src/app/dragon-animation/dragon-animation.component.ts
+++ src/app/dragon-animation/dragon-animation.component.ts
@@ -10,10 +10,14 @@ import { CommonModule } from '@angular/common';
 })
 export class DragonAnimationComponent implements OnChanges {
   @Input() trigger = 0;
+  @Input() stationary = false;
+  @Input() stationaryInvert = false;
+
 
   visible = false;
 
   ngOnChanges(): void {
+    if (this.stationary || this.stationaryInvert) return;
     if (this.trigger > 0) {
       this.visible = true;
       setTimeout(() => (this.visible = false), 2400);
