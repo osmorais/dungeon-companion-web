3e88011ce6b74b0649383a062d9811ee3eb0e19b
osmorais <osmorais>
2026-05-13 01:53:43 -0300

    add googleid no environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://dungeon-companion-api.onrender.com',
-  googleClientId: '',
+  googleClientId: '397938920552-miskpukd3ct26d86bjrl3ece88hteo2p.apps.googleusercontent.com',
 };
